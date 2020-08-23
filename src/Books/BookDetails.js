4d7d839dca754f6f4a7b1c36292bf9c3520a5a8f
Isis Moran <imoran@mail.ccsf.edu>
2020-08-22 21:31:47 -0700

    displaying book details

--- src/Books/BookDetails.js
+++ src/Books/BookDetails.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
+import './book.css';
 
 
 export class BookDetails extends Component {
@@ -18,18 +20,39 @@ export class BookDetails extends Component {
         });
     }
 
+    getAuthors() {
+        return this.state.bookData ? this.state.bookData.volumeInfo.authors.map((author) => author).join(", ") : "";
+    }
+
 
     render() {
         const { title, subtitle, description } = this.state.bookData ? this.state.bookData.volumeInfo : '';
+        const authors = this.getAuthors();
         return this.state.bookData ? (
-            <div>
-                <img src={this.state.bookData.volumeInfo.imageLinks.medium}/>
-                <h1>{title}</h1>
-                <h2>{subtitle}</h2>
-                <p>{description}</p>
-            </div>
+            <>
+                <h1 className="book-title">{title}</h1>
+                <BookDetailContainer>
+                    <div>
+                        <img src={this.state.bookData.volumeInfo.imageLinks.small}/>
+                    </div>
+                    <BookDetailColumn>
+                        <h2>{subtitle}</h2>
+                        <p>{authors}</p>
+                        <p>{description}</p>
+                    </BookDetailColumn>
+                </BookDetailContainer>
+            </>
         ) : "";
     }
 }
+
+const BookDetailContainer = styled.div`
+    display: flex;
+    padding: 0px 170px 0 170px;
+`;
+
+const BookDetailColumn = styled.div`
+    padding: 50px;
+`;
 
 export default BookDetails;
